c43a8cb7abbed5fee9747b042c9ed3103eaedabd
Alejandro <ale.acc@hotmail.com>
2019-03-20 15:57:26 +0100

    added images download service

--- src/PAL.py
+++ src/PAL.py
@@ -4,22 +4,23 @@ import rospy
 import rosbag
 
 import socket
+import ftplib
 import gphoto2 as gp
 import threading
-import sys
-import os.path
+import sys, os, os.path
 import csv
 import time
 import datetime
 
 from std_msgs.msg import Float64, Int32, Time
 from sensor_msgs.msg import Image, NavSatFix
-from inspector_software_uav.srv import RecordBagService, CameraCaptureService
+from inspector_software_uav.srv import RecordBagService, CameraCaptureService, DownloadService
 
 # Thermal camera IP address & TCP port
 WIRIS_IP = '10.0.2.228'
 WIRIS_TCP_PORT = 2240
-
+WIRIS_USER = 'wiris'
+WIRIS_PASSWORD = ''
 class PAL:
 
     def __init__(self):
@@ -51,8 +52,8 @@ class PAL:
         if not os.path.exists(self.rgb_images_dir):
             os.makedirs(self.rgb_images_dir)
         
-        if not os.path.exists(self.thermal_images_dir):
-            os.makedirs(self.thermal_images_dir)
+        if not os.path.exists(self.thermal_images_dir + 'logs/'):
+            os.makedirs(self.thermal_images_dir + 'logs/')
         
         if not os.path.exists(self.telemetry_files_dir):
             os.makedirs(self.telemetry_files_dir)
@@ -72,6 +73,7 @@ class PAL:
         
         ## WIRIS thermal camera
         thermal_camera_capture_srv = rospy.Service('thermal_camera_capture_service', CameraCaptureService, self.thermal_camera_capture_service_cb)
+        thermal_camera_download_srv = rospy.Service('thermal_camera_download_service', DownloadService, self.thermal_camera_download_service_cb)
 
         ## RGB camera
         rgb_camera_capture_srv = rospy.Service('rgb_camera_capture_service', CameraCaptureService, self.rgb_camera_capture_service_cb)
@@ -133,36 +135,54 @@ class PAL:
             self.webcam_record = req.record
             self.webcam_bag.close()
         return True
-
-    def thermal_camera_capture_service_cb(self, req):
-        sleep_time = 1/req.rate
-        if req.capture == True:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((WIRIS_IP, WIRIS_TCP_PORT))
-            s.send(HIWS)
-            if s.recv == 'OK':
-                rospy.loginfo("WIRIS Camera OK. Starting secuence capture")
-                while True:
-                    s.send(CIMG)
-                    rospy.sleep(sleep_time)
-            else:
-                rospy.logerr("ERROR. WIRIS Connection Failed")
-        if req.capture == False:
-            s.close()
         
     def rgb_camera_capture_service_cb(self, req):
         self.rgb_capture = req.capture
         if req.capture == True:
             rate = req.rate
-            self.rgb_thread = threading.Thread(target=self.rgb_camera_capture_thread, args=(rate,))
-            self.rgb_thread.start()
+            self.rgb_camera_thread = threading.Thread(target=self.rgb_camera_capture_thread, args=(rate,))
+            self.rgb_camera_thread.start()
             rospy.loginfo ('starting RGB camera capture thread')
 
-        if req.capture == False:
-            rospy.loginfo("RGB camera capturing stopped")
+        elif req.capture == False:
+            rospy.loginfo("RGB camera capture stopped")
 
         return True
 
+    def thermal_camera_capture_service_cb(self, req):
+        self.thermal_capture = req.capture
+        if req.capture == True:
+            rate = req.rate
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s.connect((WIRIS_IP, WIRIS_TCP_PORT))
+            self.s.send('HIWS')
+            connect_resp = self.s.recv(3)
+            if connect_resp == 'OK':
+                self.s.send('SPET')
+                rospy.loginfo("WIRIS Camera OK. Starting secuence capture")
+                thermal_camera_thread = threading.Thread(target=self.thermal_camera_capture_thread, args=(rate,))
+                thermal_camera_thread.start()
+            else:
+                rospy.logerr("ERROR. WIRIS Connection Failed")
+        elif req.capture == False:
+            try:
+                self.s
+                s_exist = True
+            except NameError:
+                s_exist = False
+            if s_exist:
+                self.s.close()
+            rospy.loginfo('Thermal camera capture stopped')
+        
+        return True
+
+    def thermal_camera_download_service_cb(self, req):
+        try:
+            self.thermal_images_download()
+            return True
+        except:
+            rospy.logerr("Can't download files from thermal camera")
+            return False
     
     ## Cameras capture Threads
 
@@ -191,11 +211,21 @@ class PAL:
                     rospy.logerr("ERROR: RGB camera connection failed")
                 rgb_file.close()
                 
-
             gp.check_result(gp.gp_camera_exit(self.rgb_camera))  
             rospy.loginfo ('exit RGB camera capture thread')
             sys.exit()
         
+    def thermal_camera_capture_thread(self, rate):
+        while self.thermal_capture:
+            self.s.send('CIMG')
+            capt_resp = self.s.recv(3)
+            if capt_resp == 'OK':
+                rospy.loginfo('Capturing Thermal Image')
+            else:
+                rospy.logerr("Can't capture Thermal Image")
+            rospy.sleep(1/rate)
+        rospy.loginfo('exit Thermal camera capture thread')
+
     ## Capture functions
 
     def rgb_camera_capture_function(self):
@@ -212,6 +242,50 @@ class PAL:
         rostime = Time()
         rostime.data = rospy.get_rostime()
         self.rgb_file_writer.writerow({'RGB Image':filename, 'Time':rospy.get_rostime()})
+
+    ## Thermal images download function
+
+    def thermal_images_download(self):
+        ftp = ftplib.FTP(WIRIS_IP)
+        ftp.login(WIRIS_USER, WIRIS_PASSWORD)
+        directorylist = ftp.nlst()
+        rospy.loginfo('Downloading files from thermal camera')
+        print('directory list: ', directorylist)
+        for directory in directorylist:
+            if directory == 'logs':
+                print '\nlogs:'
+                filenames = ftp.nlst(directory)
+                print filenames
+                for filename in filenames:
+                    local_filename = self.thermal_images_dir + 'logs/' + filename
+                    f = open(local_filename, 'wb')
+                    ftp.retrbinary('RETR '+ 'logs/' + filename, f.write)
+                    f.close()
+                    print ('downloaded: ' + filename)
+            else:
+                subdirlist = ftp.nlst(directory)
+                print('\ndirectory: ' + directory)
+                print('subdirectories: ' + str(subdirlist))
+                print('imgs:')
+                img_dir = directory + '/img/'
+                filenames = ftp.nlst(img_dir)
+                for filename in filenames:
+                    # local_filename = os.path.join(self.thermal_images_dir, filename)
+                    local_filename = self.thermal_images_dir + filename
+                    f = open(local_filename, 'wb')
+                    ftp.retrbinary('RETR '+ img_dir + filename, f.write)
+                    f.close()
+                    print ('downloaded: ' + filename)
+        rospy.loginfo('Download finished')
+        ftp.quit()
+        
+            # for file in files:
+                # print file
+                # try:
+                #     ftp.retrbinary("RETR " + file, open(self.thermal_images_dir + file,"wb").write)
+                #     print "Downloaded: " + file
+                # except:
+                #     print "Error: File could not be downloaded " + file
 
 ## end PAL Class ##
 
